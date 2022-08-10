f3e5cc485b4930e446739285e5527a45bf079546
Bhavya Giri <bhavyagiri@Bhavyas-MacBook-Pro.local>
2022-08-10 08:04:09 +0530

    second-commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBIiCRPbm1T3KTFpmN3ZAOwDJ_ynIIcQrA",
+    authDomain: "netflix-build-5f1a5.firebaseapp.com",
+    projectId: "netflix-build-5f1a5",
+    storageBucket: "netflix-build-5f1a5.appspot.com",
+    messagingSenderId: "36218287487",
+    appId: "1:36218287487:web:ed6c55ffb70216abafd64a"
+  };
